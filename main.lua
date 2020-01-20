b815b69bd9ebd2d10e74a79269f7aefe54b1b7eb
willroy <willjoroy@gmail.com>
2020-01-20 20:40:58 +0000

    initial commit

---
+++ main.lua
@@ -0,0 +1,18 @@
+function love.load()
+  love.window.setMode(1300, 1000)
+  love.graphics.setBackgroundColor(1,1,1)
+  start = false
+end
+
+function love.update(dt)
+end
+
+function love.draw()
+end
+
+
+function love.mousepressed(x, y, button, istouch)
+end
+
+function love.keypressed(key, scancode, isrepeat)
+end
