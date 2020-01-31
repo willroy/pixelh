a22dae8ef8eb1203152ef30df0210ea2d8cad0d8
willroy <willjoroy@gmail.com>
2020-01-31 04:12:27 +0000

    Split into separate files

--- src/player.lua
+++ src/player.lua
@@ -2,7 +2,7 @@ local O = {}
 
 local function load()
   x = 10
-  y = 510
+  y = 480
   acc = 6
   grav = 2
   ac = false
@@ -10,34 +10,47 @@ local function load()
   wc = false
   sc = false
   fall = false
+  image = love.graphics.newImage("assets/slime.png")
+  quad1 = love.graphics.newQuad(0, 0, 20, 20, image:getWidth(), image:getHeight())
+  quad2 = love.graphics.newQuad(20, 0, 20, 20, image:getWidth(), image:getHeight())
+  quad3 = love.graphics.newQuad(40, 0, 20, 20, image:getWidth(), image:getHeight())
+  quad4 = love.graphics.newQuad(60, 0, 20, 20, image:getWidth(), image:getHeight())
+  quad5 = love.graphics.newQuad(0, 40, 20, 20, image:getWidth(), image:getHeight())
+  quad6 = love.graphics.newQuad(20, 40, 20, 20, image:getWidth(), image:getHeight())
+  walk = {quad1, quad2, quad3, quad4}
+  idle = {quad5, quad6}
+  p = {10, 480, image}
+  count = 0
+  frame = 1
+  counti = 0
+  framei = 1
 end
 
 local function update()
   ac, dc, wc = false
-  if collision(x-3, y, platforms) == false and love.keyboard.isDown("a","left") then x = x - 3 end
-  if collision(x+3, y, platforms) == false and love.keyboard.isDown("d","right") then x = x + 3 end
+  
+  if collision(p[1]-3, p[2], platforms) == false and love.keyboard.isDown("a","left") then p[1] = p[1] - 3 end
+  if collision(p[1]+3, p[2], platforms) == false and love.keyboard.isDown("d","right") then p[1] = p[1] + 3 end
   
   if love.keyboard.isDown("a","left") then ac = true end
   if love.keyboard.isDown("d","right") then dc = true end
   if love.keyboard.isDown("s","down") then sc = true end
   
-  if love.keyboard.isDown("w","up") and fall == false then
-    jump = true
-    wc = true
-  end
+  if love.keyboard.isDown("w","up") and fall == false then jump = true end
+  if love.keyboard.isDown("w","up") then wc = true end
   
   if jump then
-    if collision(x, y-(acc-0.2), platforms) == false and y-(acc-0.2) < 510 then 
+    if collision(p[1], p[2]-(acc-0.2), platforms) == false and p[2]-(acc-0.2) < 480 then 
       acc = acc - 0.2 
-      y = y - acc 
+      p[2] = p[2] - acc 
     else 
       jump = false 
       acc = 6 
     end
   end
    
-  if collision(x, y+grav, platforms) == false and (y+grav) < 510 then 
-    y = y + grav 
+  if collision(p[1], p[2]+grav, platforms) == false and (p[2]+grav) < 480 then 
+    p[2] = p[2] + grav 
     fall = true
   else fall = false end
 end
@@ -52,8 +65,25 @@ local function draw()
   if ac then love.graphics.rectangle("fill", 1090, 900, 50, 50) end
   if wc then love.graphics.rectangle("fill", 1150, 840, 50, 50) end
   if dc then love.graphics.rectangle("fill", 1210, 900, 50, 50) end
-  love.graphics.setColor(0,0,0)
-  love.graphics.rectangle("fill", x, y, 10, 10)
+  love.graphics.setColor(1,1,1)
+  love.graphics.rectangle("fill", p[1], p[2], 10, 10)
+  if ac or dc then 
+    count = count + 1
+    if count == 20 then
+      frame = frame + 1
+      count = 0
+    end
+    love.graphics.draw(image, walk[frame], p[1], p[2], 0, 2, 2)
+    if frame == 4 then frame = 1 end
+  else
+    counti = counti + 1
+    if counti == 40 then
+      if framei == 2 then framei = 1
+      else framei = framei + 1 end
+      counti = 0
+    end    
+    love.graphics.draw(image, idle[framei], p[1], p[2], 0, 2, 2)
+  end
 end
 
 O.load = load
--- src/room.lua
+++ src/room.lua
@@ -2,25 +2,16 @@ local O = {}
 
 local function load()
   platforms = {{100, 500, 50, 20}, {300, 500, 50, 20}, {200, 470, 50, 30}}
-  mplatforms = {{400, 470, 50, 30, true}}   
+
 end
 
 local function update()
-  for i=1,#mplatforms do
-    if platforms[i][5] then
-      if mplatforms[i][1] < 1100 or collision(mplatforms[i][1]+10, mplatforms[i][2], platforms) == false then
-        mplatforms[i][1] = mplatforms[i][1] + 10
-      elseif platforms[i][1] > 100 or collision(mplatforms[i][1]+10, mplatforms[i][2], platforms) == false then
-        mplatforms[i][1] = mplatforms[i][1] - 10
-      end
-    end
-  end
 end
 
 function collision(xnew, ynew, objects)
   for i=1,#objects do
-    if xnew+10 > objects[i][1] and xnew < (objects[i][1]+objects[i][3]) then
-      if ynew+10 > objects[i][2] and ynew < (objects[i][2]+objects[i][4]) then
+    if xnew+40 > objects[i][1] and xnew < (objects[i][1]+objects[i][3]) then
+      if ynew+40 > objects[i][2] and ynew < (objects[i][2]+objects[i][4]) then
         return true
       end
     end
@@ -33,9 +24,6 @@ local function draw()
   love.graphics.setColor(0,0,1)
   for i=1,#platforms do
     love.graphics.rectangle("fill", platforms[i][1], platforms[i][2], platforms[i][3], platforms[i][4])
-  end
-  for i=1,#mplatforms do
-    love.graphics.rectangle("fill", mplatforms[i][1], mplatforms[i][2], mplatforms[i][3], mplatforms[i][4])
   end
   love.graphics.setColor(0,0,0)
   love.graphics.rectangle("fill", 0, 520, 1200, 3)
